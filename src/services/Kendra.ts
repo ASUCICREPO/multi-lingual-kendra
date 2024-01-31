dc677b67f0f3580e746e58c568c4027beea8398b
Anjali Srivastava <anjali.pihu@gmail.com>
2024-01-31 11:41:56 -0700

    update config

--- src/services/Kendra.ts
+++ src/services/Kendra.ts
@@ -16,12 +16,12 @@ try {
   );
 }
 
-// const config = {
-//   accessKeyId: process.env.REACT_APP_ACCESS_ID,
-//   secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
-//   region: process.env.REACT_APP_REGION,
-//   indexId: process.env.REACT_APP_INDEXID
-// }
+config = {
+  accessKeyId: process.env.REACT_APP_ACCESS_ID,
+  secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+  region: process.env.REACT_APP_REGION,
+  indexId: process.env.REACT_APP_INDEXID
+}
 
 if (config) {
   if (!config.accessKeyId) {
